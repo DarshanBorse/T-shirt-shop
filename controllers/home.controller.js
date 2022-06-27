71dc70c2aa1d60bbd7ae22f2b114ba9e08b0f00b
DarshanBorse <borsedarshan77@gmail.com>
2022-06-27 13:22:14 +0530

    Updated

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,9 +1,11 @@
-exports.home = (req, res) => {
-  return res.status(200).json({
+const bigPromise = require("./../middleware/bigPromise");
+
+exports.home = bigPromise(async (req, res) => {
+  res.status(200).json({
     success: true,
     greeting: "Hello from api",
   });
-};
+});
 
 exports.homeDummy = (req, res) => {
   return res.status(200).json({
